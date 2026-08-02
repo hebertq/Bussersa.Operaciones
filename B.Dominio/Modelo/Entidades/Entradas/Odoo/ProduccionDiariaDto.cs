8ebb48cfe9b328a8db5474aab2d68eb42150f3a4
hjquinteroc1 <hjquinteroc@gmail.com>
2026-08-02 10:52:48 -0600

    feat(produccion): agregar columna de herencia en tabla de Produccion Diaria

--- B.Dominio/Modelo/Entidades/Entradas/Odoo/ProduccionDiariaDto.cs
+++ B.Dominio/Modelo/Entidades/Entradas/Odoo/ProduccionDiariaDto.cs
@@ -30,5 +30,8 @@ namespace Modelo.Entidades.Entradas.Odoo
         public int usuario_registro { get; set; }
         public int? proforma_orden { get; set; }
         public int? operacion_id { get; set; }
+        public int? parent_id { get; set; }
+        public string? parent_producto_nombre { get; set; }
     }
 }
+
